c12f2ea6ddca0b73d8887413f6fc36332610385b
Maggie Leavell <mjleavell@gmail.com>
2018-10-22 19:39:45 -0500

    boards click event complete

---
+++ javascripts/components/boards.js
@@ -0,0 +1,43 @@
+import { loadBoards } from "../data/boardsData.js";
+
+const bindEvents = () => {
+    // board click event
+    $('#user-boards').on('click', '.board-card', (e) => {
+        const clickedBoardId = $(e.target).closest('.board-card').attr('id');
+        $('#boards-page').hide();
+        $('#pins-page').show();
+        console.log(clickedBoardId);
+    })
+    // back to boards click event
+    $('#pins-page').on('click', '#toBoardsBtn', () => {
+        $('#pins-page').hide();
+        $('#boards-page').show();
+    })
+}
+
+const writeBoards = (boards) => {
+    let domString = '';
+    boards.forEach((board) => {
+        domString += `
+        <div id="${board.id}" class="board-card p-2">
+            <img class="card-img-top" src="./db/default-img.jpeg" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">${board.name}</h5>
+                <p class="card-text">42 Pins</p>
+            </div>
+        </div>
+        `
+    })
+    $('#user-boards').html(domString);
+}
+
+const initialBoardView = () => {
+    loadBoards().then((boards) => {
+        writeBoards(boards);
+        bindEvents();
+    }).catch((error) => {
+        console.log(error);
+    })
+}
+
+export {initialBoardView}
